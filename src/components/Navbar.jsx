9be00862d443c82a38dd94cb14e87827ea73f614
MisterDoom <74387747+MisterDoom@users.noreply.github.com>
2023-12-14 19:25:29 -0500

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -39,7 +39,7 @@ function Navbar() {
       <div className="navbar-center">
         <Link to="/">
           <div className="btn btn-ghost normal-case text-xl">
-            <FontAwesomeIcon icon={fas.faMugHot} /> Mocha
+            <FontAwesomeIcon icon={fas.faMugHot} /> Landins Proxy
           </div>
         </Link>
       </div>
